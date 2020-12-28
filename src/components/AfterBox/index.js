5997af0b8f7fe1e1475749d4bf5444407a5dd5ba
SvetlanaM <richard.margeta@gmail.com>
2020-12-28 17:43:38 +0100

    update'

--- src/components/AfterBox/index.js
+++ src/components/AfterBox/index.js
@@ -4,11 +4,6 @@ const AfterBox = () => {
 	return (
 		<div className="after-box">
 			<h1>Nový rok 2021 je už tu. Tak si ho naplno uži.</h1>
-			<iframe
-				width="560"
-				height="315"
-				src="https://www.youtube.com/embed/6ZGRDRf4nPU"
-				frameBorder="0"></iframe>
 		</div>
 	);
 };
