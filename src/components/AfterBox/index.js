099b2b9a747611aedaf399adcbbd004769fb96bb
SvetlanaM <richard.margeta@gmail.com>
2021-01-04 13:56:52 +0100

    Add alt property to img element

--- src/components/AfterBox/index.js
+++ src/components/AfterBox/index.js
@@ -4,7 +4,10 @@ const AfterBox = () => {
 	return (
 		<div className="after-box">
 			<h1>Nový rok 2021 je už tu. Tak si ho naplno uži.</h1>
-			<img src="https://media.giphy.com/media/l41Yk0Ovlxko5oYco/giphy.gif" />
+			<img
+				src="https://media.giphy.com/media/l41Yk0Ovlxko5oYco/giphy.gif"
+				alt="New Year dancing cat"
+			/>
 		</div>
 	);
 };
