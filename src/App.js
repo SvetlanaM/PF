85d2a6c1b18681622477fc2ba6eeee62f20dd290
SvetlanaM <svetlana@kompilator.cz>
2022-03-08 18:23:13 +0100

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,87 +1,70 @@
-import React, { useRef } from 'react';
-import Button from './components/Button';
-import Layout from './components/Layout';
-import MainBox from './components/MainBox';
-import OldYearBox from './components/OldYearBox';
-import SendBox from './components/SendBox';
-import './App.css';
-import { generateTitle, getName, getRandomYearTip } from './utils/helpers';
-import { useTip } from './hooks/useTip';
-import { useFrom } from './hooks/useFrom';
-import { baseUrl, curDate, tips } from './utils/Constants';
-import SideGrid from './components/SideGrid';
+import React, { useRef } from 'react'
+import Button from './components/Button'
+import Layout from './components/Layout'
+import MainBox from './components/MainBox'
+import OldYearBox from './components/OldYearBox'
+import './App.css'
+import { generateTitle, getName, getRandomYearTip } from './utils/helpers'
+import { useTip } from './hooks/useTip'
+import { useFrom } from './hooks/useFrom'
+import { baseUrl, curDate, tips } from './utils/Constants'
+import SideGrid from './components/SideGrid'
 
 function App() {
-	const title = generateTitle();
-	const nameInput = useRef(null);
-	const [from, setFrom] = useFrom('');
-	const [tip, setTip] = useTip(getRandomYearTip(tips, '???'));
-	const newUrl = baseUrl + '?name=' + from;
+  const [from, setFrom] = useFrom('')
+  const [tip, setTip] = useTip(getRandomYearTip(tips, '???'))
 
-	const handleClick = () => {
-		setTip(getRandomYearTip(tips, tip));
-	};
+  const title = generateTitle()
+  const nameInput = useRef(null)
+  const newUrl = baseUrl + '?name=' + from
 
-	const handleChange = (e) => {
-		setFrom(e.target.value);
-	};
+  const handleClick = () => {
+    setTip(getRandomYearTip(tips, tip))
+  }
 
-	const onShareWindowClose = () => {
-		alert(
-			'Tvoja správa bola úspešne nazdieľaná kamarátom. Možeš zdieľať ďalej pod inými menami (nieže by to malo zmysel :D)'
-		);
-		setFrom('');
-		nameInput.current.focus();
-	};
+  const handleChange = (e) => {
+    setFrom(e.target.value)
+  }
 
-	return (
-		title && (
-			<Layout>
-				<OldYearBox title={title} />
-				<MainBox
-					name={getName()}
-					year={curDate.getFullYear() + 1}
-					tip={
-						tip
-							? tip
-							: 'záhadný a ani náš generátor ti už nevie poradiť. Nechaj sa prekvapiť.'
-					}
-				/>
-				{tip ? (
-					<Button
-						className='btn-generate'
-						label={tip === '???' ? 'Generovať' : 'Generovať znovu'}
-						onClick={handleClick}></Button>
-				) : null}
+  const onShareWindowClose = () => {
+    alert(
+      'Tvoja správa bola úspešne nazdieľaná kamarátom. Možeš zdieľať ďalej pod inými menami (nieže by to malo zmysel :D)'
+    )
+    setFrom('')
+    nameInput.current.focus()
+  }
 
-				<section id='send'>
-					<span className='info'>
-						Napíš svoje meno a zdieľaj personalizované PFko pre kamarátov na
-						Facebooku :)
-					</span>
-					<div id='send-next'>
-						<SideGrid className='left-side'>
-							<input
-								type='text'
-								value={from}
-								onChange={handleChange}
-								className='input-name'
-								placeholder='Zadaj svoje meno'
-								ref={nameInput}
-							/>
-						</SideGrid>
-						<SideGrid className='right-side'>
-							<SendBox
-								url={newUrl}
-								hashtag={`#pffrom${from}`}
-								onShareWindowClose={onShareWindowClose}
-							/>
-						</SideGrid>
-					</div>
-				</section>
-			</Layout>
-		)
-	);
+  return (
+    title && (
+      <Layout>
+        <OldYearBox title={title} />
+        <MainBox
+          name={getName()}
+          year={curDate.getFullYear() + 1}
+          tip={
+            tip
+              ? tip
+              : 'záhadný a ani náš generátor ti už nevie poradiť. Nechaj sa prekvapiť.'
+          }
+        />
+        {tip ? (
+          <Button
+            className="btn-generate"
+            label={tip === '???' ? 'Generovať' : 'Generovať znovu'}
+            onClick={handleClick}
+          ></Button>
+        ) : null}
+
+        <SideGrid
+          from={from}
+          handleChange={handleChange}
+          newUrl={newUrl}
+          onShareWindowClose={onShareWindowClose}
+          nameInput={nameInput}
+        />
+      </Layout>
+    )
+  )
 }
 
-export default App;
+export default App
--- src/components/SideGrid/index.js
+++ src/components/SideGrid/index.js
@@ -1,5 +1,33 @@
-const SideGrid = ({ className, children }) => {
-	return <div className={className}>{children}</div>;
+import SendBox from '../SendBox/index'
+
+const SideGrid = ({ from, handleChange, newUrl, onShareWindowClose, nameInput }) => {
+	return (
+		<section id='send'>
+			<span className='info'>
+				Napíš svoje meno a zdieľaj personalizované PFko pre kamarátov na
+				Facebooku :)
+			</span>
+			<div id='send-next'>
+				<div className='left-side'>
+					<input
+						type='text'
+						value={from}
+						onChange={handleChange}
+						className='input-name'
+						placeholder='Zadaj svoje meno'
+						ref={nameInput}
+					/>
+				</div>
+				<div className='right-side'>
+					<SendBox
+						url={newUrl}
+						hashtag={`#pffrom${from}`}
+						onShareWindowClose={onShareWindowClose}
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default SideGrid;
